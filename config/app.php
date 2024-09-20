2eeb0f5f208a1713115f17c6b90c88530496ad29
FernandoYZ <fernandozavala266@gmail.com>
2024-09-20 17:27:45 -0500

    feat(config): Agregar array para app
     - Se está retornando un array con los datos de la variable de entorno para los campos 'name', 'env', debug', 'url', 'currency' y 'language'
     - Si no encuentra datos en .env toma datos por defecto que se han agregado manualmente.
    
    No hay issues
    #Close 0

---
+++ config/app.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'name' => env('APP_NAME', 'Nombre_proyecto'),
+    'env' => env('APP_ENV', 'base_de_datos'),
+    'debug' => filter_var(env('APP_DEBUG', false), FILTER_VALIDATE_BOOLEAN),
+    'url' => env('APP_URL', 'http://localhost'),
+    'currency' => env('APP_CURRENCY', 'PEN'),
+    'language' => env('APP_LANGUAGE', 'es'),
+];
