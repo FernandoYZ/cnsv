769e22035003535a3e116358b4d7135467f64464
FernandoYZ <fernandozavala266@gmail.com>
2024-09-21 03:23:04 -0500

    feat(views): Crear vista de error 500
    - Se ha creado el archivo `500.php` en la carpeta `resources/Views` para mostrar una página de error 500 personalizada.
    - La vista incluye:
      - Estructura HTML básica con metadatos y enlaces a estilos CSS, incluyendo fuentes de Google y Font Awesome.
      - Un mensaje de error 500 con un SVG representativo.
      - Un mensaje amigable que informa al usuario sobre el error y sugiere intentar nuevamente más tarde o contactar al soporte.
      - Enlaces para regresar a la página de inicio y contactar con soporte, cada uno con un icono SVG.
    
    No hay issues
    #Close 0

---
+++ resources/Views/Errors/500.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Error 500">
+    <title>Error 500</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="<?= assets('app.css') ?>">
+</head>
+<body>
+    <main>
+        <h1>500</h1>
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+            <path d="m8 2 1.88 1.88"></path>
+            <path d="M14.12 3.88 16 2"></path>
+            <path d="M9 7.13v-1a3.003 3.003 0 1 1 6 0v1"></path>
+            <path d="M12 20c-3.3 0-6-2.7-6-6v-3a4 4 0 0 1 4-4h4a4 4 0 0 1 4 4v3c0 3.3-2.7 6-6 6"></path>
+            <path d="M12 20v-9"></path>
+            <path d="M6.53 9C4.6 8.8 3 7.1 3 5"></path>
+            <path d="M6 13H2"></path>
+            <path d="M3 21c0-2.1 1.7-3.9 3.8-4"></path>
+            <path d="M20.97 5c0 2.1-1.6 3.8-3.5 4"></path>
+            <path d="M22 13h-4"></path>
+            <path d="M17.2 17c2.1.1 3.8 1.9 3.8 4"></path>
+        </svg>
+        <p>Oops, Algo salió mal!</p>
+        <p>Lo sentimos, pero se ha producido un error inesperado por nuestra parte. Inténtelo de nuevo más tarde o comuníquese con nuestro equipo de soporte.</p>
+        <a href="/">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+                <polyline points="9 22 9 12 15 12 15 22"></polyline>
+            </svg>
+            Regresar al Inicio
+        </a>
+        <a href="/">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <path d="M12 2v10"></path>
+                <path d="M18.4 6.6a9 9 0 1 1-12.77.04"></path>
+            </svg>
+            Contactar con Soporte
+        </a>
+    </main>
+</body>
+</html>
