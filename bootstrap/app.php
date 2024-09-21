49cc25408c43398cdfd3bfce2aec37ee41fb300b
FernandoYZ <fernandozavala266@gmail.com>
2024-09-21 02:28:42 -0500

    feat(bootstrap): Crear archivo de bootstrap para inicializar la aplicación
    - Se ha creado el archivo `app.php` en la carpeta `bootstrap` para gestionar la inicialización de la aplicación.
    - El archivo incluye:
      - Carga automática de dependencias utilizando Composer.
      - Carga de configuraciones desde archivos `.env` y de configuración de la aplicación y base de datos.
      - Creación de instancias de las clases `Database`, `Views`, `Router` y `ExceptionHandler`.
      - Configuración del manejador de excepciones para el enrutador.
      - Inclusión de las rutas desde el archivo `web.php`.
    - Se ha implementado la clase `App` para manejar la configuración, el enrutador y la base de datos, con un método `handle` para procesar las solicitudes.
    
    No hay issues
    #Close 0

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,2 +1,51 @@
 <?php
 
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../app/helpers.php';
+
+use Core\Router;
+use Core\Database;
+use Core\Views;
+use Core\ExceptionHandler;
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+$config = require_once __DIR__ . '/../config/app.php';
+$dbConfig = require_once __DIR__ . '/../config/database.php';
+
+$GLOBALS['config'] = array_merge($config, ['database' => $dbConfig]);
+
+$databaseConfig = $GLOBALS['config']['database'];
+
+$database = new Database($databaseConfig);
+$views = new Views();
+$router = new Router($database, $views);
+$exceptionHandler = new ExceptionHandler();
+$router->setExceptionHandler($exceptionHandler);
+
+// $routes = ['web.php', 'api.php', 'auth.php'];
+// foreach ($routes as $routeFile) {
+//     require_once __DIR__ . '/../routes/' . $routeFile;
+// }
+require_once __DIR__ . '/../routes/web.php';
+
+$app = new App($GLOBALS['config'], $router, $database);
+return $app;
+
+class App {
+    protected $config;
+    protected $router;
+    protected $database;
+
+    public function __construct($config, $router, $database) {
+        $this->config = $config;
+        $this->router = $router;
+        $this->database = $database;
+    }
+
+    public function handle($request) {
+        $this->router->route($request);
+    }
+}
