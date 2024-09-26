95a3615fa0b6964b067bf27f80afceee6f581aaa
FernandoYZ <fernandozavala266@gmail.com>
2024-09-26 14:18:25 -0500

    feat(about): agregar datos de título y descripción en el controlador Se actualiza el método index en AboutController para incluir datos adicionales al renderizar la vista.
    - Se añade un array  que contiene:
      - 'title': '¿Quiénes somos?'
      - 'description': 'Conoce el Colegio Nuestra Señora del Valle, donde promovemos una educación integral y valores en un ambiente acogedor y familiar.'
    
    Estos cambios permiten que la vista sobre la página 'Sobre nosotros' tenga un contenido más significativo y estructurado.
    
    No hay issues
    #Close 0

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -6,7 +6,10 @@ use Core\Controller;
 
 class AboutController extends Controller {
     public function index() {
-        $data = [];
+        $data = [
+            'title' => '¿Quiénes somos?',
+            'description' => 'Conoce el Colegio Nuestra Señora del Valle, donde promovemos una educación integral y valores en un ambiente acogedor y familiar.'
+        ];
         return $this->views->getView('Pages', 'about', $data);
     }
 }
