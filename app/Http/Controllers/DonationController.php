2b1954a6a418e9db1300639476cf1456049b7e01
FernandoYZ <fernandozavala266@gmail.com>
2024-09-26 14:19:42 -0500

    feat(donation): agregar datos de título y descripción en el controlador Se actualiza el método index en DonationController para incluir datos adicionales al renderizar la vista.
    - Se añade un array  que contiene:
      - 'title': 'Donaciones'
      - 'description': 'Apoya al Colegio Nuestra Señora del Valle con tu donación y ayuda a fortalecer la educación integral de nuestros estudiantes.'
    
    Estos cambios permiten que la vista sobre la página de donaciones tenga un contenido más informativo y atractivo para los donantes.
    
    No hay issues
    #Close 0

--- app/Http/Controllers/DonationController.php
+++ app/Http/Controllers/DonationController.php
@@ -6,7 +6,10 @@ use Core\Controller;
 
 class DonationController extends Controller {
     public function index() {
-        $data = [];
+        $data = [
+            'title' => 'Donaciones',
+            'description' => 'Apoya al Colegio Nuestra Señora del Valle con tu donación y ayuda a fortalecer la educación integral de nuestros estudiantes.',
+        ];
         return $this->views->getView('Pages', 'donation', $data);
     }
 }
