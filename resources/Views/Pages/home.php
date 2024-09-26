bd36a4b38f792affe0a3176a3d9fba8a6b861b65
FernandoYZ <fernandozavala266@gmail.com>
2024-09-26 16:37:31 -0500

    feat(views): añadir estructura de la página principal 'Home' Se implementa la estructura de la página 'Home', que incluye múltiples secciones y funciones para mejorar la presentación del contenido. Los elementos clave son:
    - HeroHome: Se añade una sección destacada con la función HeroHome,
      que utiliza la imagen 'hero/home_hero.jpg' y el título 'Proceso de
      admisión 2024'. Incluye un botón de acción 'Ir al Formulario',
      facilitando la navegación a los usuarios interesados en el proceso de
      admisión.
    
    - Sección de servicios: Se crea una sección con ID 'servicios', que
      presenta un título 'Nuestros Servicios'. Esta sección está diseñada
      para resaltar los servicios ofrecidos por la institución, utilizando
      un fondo gris claro.
    
    - Sección 'Acerca de Nosotros': Se añade otra sección con ID 'acerca',
      que incluye el título 'Acerca de Nosotros'. Esta sección proporciona
      información adicional sobre la institución y su misión, utilizando
      un fondo gris medio.
    
    - Sección de contacto: Se incorpora una sección con ID 'contacto',
      que presenta el título 'Contacto'. Esta sección ofrece opciones para
      que los usuarios se comuniquen con la institución, utilizando un fondo
      gris oscuro.
    
    - HeroSection: Al final de la página, se añade una sección Hero con
      la función HeroSection, que utiliza la imagen 'section_bg.jpg' y el
      texto 'Envíanos tu apoyo y sé parte del crecimiento de nuestra
      institución', con un botón de acción 'Donar'.
    
    Esta implementación proporciona una navegación clara y un diseño visualmente
    atractivo que mejora la experiencia del usuario en la página de inicio.
    
    No hay issues
    #Close 0

---
+++ resources/Views/Pages/home.php
@@ -0,0 +1,25 @@
+<?=
+HeroHome(
+    'hero/home_hero.jpg',
+    'Proceso de admisión 2024',
+    'Ir al Formulario'
+)
+?>
+<section id="servicios" class="h-screen bg-gray-300 flex items-center justify-center">
+    <h2 class="text-4xl">Nuestros Servicios</h2>
+</section>
+
+
+<section id="acerca" class="h-screen bg-gray-400 flex items-center justify-center">
+    <h2 class="text-4xl">Acerca de Nosotros</h2>
+</section>
+<section id="contacto" class="h-screen bg-gray-500 flex items-center justify-center">
+    <h2 class="text-4xl">Contacto</h2>
+</section>
+<?=
+HeroSection(
+    'section_bg.jpg',
+    'Envíanos tu apoyo y sé parte del crecimiento de nuestra institución',
+    'Donar'
+)
+?>
