da112694164260b10d8559b188a0c8d51921950b
FernandoYZ <fernandozavala266@gmail.com>
2024-09-26 14:19:15 -0500

    feat(admission): agregar datos de título y descripción en el controlador Se actualiza el método index en AdmissionController para incluir datos adicionales al renderizar la vista.
    - Se añade un array  que contiene:
      - 'title': 'Admisión'
      - 'description': 'Infórmate sobre el proceso de admisión en el Colegio Nuestra Señora del Valle y cómo formar parte de nuestra comunidad educativa.'
    
    Estos cambios permiten que la vista sobre la página de admisión tenga un contenido más relevante y atractivo para los usuarios.
    
    No hay issues
    #Close 0

--- app/Http/Controllers/AdmissionController.php
+++ app/Http/Controllers/AdmissionController.php
@@ -6,7 +6,10 @@ use Core\Controller;
 
 class AdmissionController extends Controller {
     public function index() {
-        $data = [];
+        $data = [
+            'title' => 'Admisión',
+            'description' => 'Infórmate sobre el proceso de admisión en el Colegio Nuestra Señora del Valle y cómo formar parte de nuestra comunidad educativa.',
+        ];
         return $this->views->getView('Pages', 'admission', $data);
     }
 }
