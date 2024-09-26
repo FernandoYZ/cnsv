504ed383a7ae6059247d51670edb5a276ceb8cb9
FernandoYZ <fernandozavala266@gmail.com>
2024-09-26 16:21:40 -0500

    feat(views): agregar sección de copyright en el pie de página Se ha añadido un nuevo componente de pie de página que incluye información de copyright.
    - Implementación de un footer que utiliza clases de TailwindCSS para el diseño responsivo y estético.
    - Muestra el año actual y el título de la propiedad, obtenido de la configuración, para personalizar el mensaje de copyright.
    - Incluye un enlace al nombre de la compañía, que también se obtiene de la configuración, con un estilo que resalta al pasar el cursor.
    - El footer está diseñado para adaptarse a diferentes tamaños de pantalla, asegurando una buena visualización en dispositivos móviles y de escritorio.
    
    Esta mejora contribuye a la presentación profesional del sitio y garantiza el cumplimiento de las normativas de derechos de autor.
    
    No hay issues
    #Close 0

---
+++ resources/Views/layouts/partials/copyright.php
@@ -0,0 +1,6 @@
+<footer class="font-roboto w-full py-6 px-4 md:px-8 lg:px-72 items-center justify-between gap-4 text-xs bg-neutral-950 text-neutral-400">
+    <p class="text-center md:text-left">
+        &copy; 2024 <?= config('property_title') ?>. Todos los derechos reservados. Realizado por 
+        <a href="#" class="font-semibold hover:transition-all hover:text-white"><?= config('company') ?></a>
+    </p>
+</footer>
