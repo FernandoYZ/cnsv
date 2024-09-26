ac6dbf976ba37f318e81096a751e2148ebe28ca6
FernandoYZ <fernandozavala266@gmail.com>
2024-09-26 16:36:16 -0500

    feat(views): añadir sección Hero en la página 'About'
    Se añade la función HeroSection en la página 'About', que permite la creación
    de una sección destacada con un fondo visual y contenido dinámico. Esta función
    proporciona los siguientes elementos:
    
    - imagen: Acepta una ruta de imagen que se utiliza como fondo de la
      sección, en este caso 'hero/about_hero.png', lo que enriquece la
      presentación visual.
    
    - título: Permite especificar un título, en este caso '¿Quiénes somos?',
      que se muestra de forma prominente en la sección.
    
    - botón de acción: Incluye un botón de texto, aquí 'Donar', que se
      estiliza para mejorar la interactividad y motivar la acción del usuario.
    
    - contenido opcional: Permite agregar contenido adicional en forma de
      párrafo. En este caso, se incluye una breve descripción de la
      institución: 'Somos una Institución Educativa de Acción Conjunta de
      Educación Básica Regular entre la Iglesia Católica y el Estado Peruano,
      erigida como Colegio – Seminario Menor'.
    
    La implementación de esta función mejora la estructura y legibilidad
    del código, permitiendo reutilización en otras secciones de la aplicación
    que requieran un diseño similar.
    
    No hay issues
    #Close 0

---
+++ resources/Views/Pages/about.php
@@ -0,0 +1,8 @@
+<?=
+HeroSection(
+    'hero/about_hero.png',
+    '¿Quiénes somos?',
+    'Donar',
+    'Somos una Institución Educativa de Acción Conjunta de Educación Básica Regular entre la Iglesia Católica y el Estado Peruano, erigida como Colegio – Seminario Menor, '
+)
+?>
