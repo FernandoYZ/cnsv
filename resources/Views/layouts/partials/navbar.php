efce8e60450f44545288411600facfb1b16b082a
FernandoYZ <fernandozavala266@gmail.com>
2024-09-26 21:29:43 -0500

    fix(navbar): mejorar accesibilidad y estructura de la barra de navegación Se actualiza el archivo de la barra de navegación para optimizar la accesibilidad y la semántica del código. Las modificaciones incluyen:
    - atributo 'aria-label': Se añade el atributo 'aria-label' al elemento
      <nav> para proporcionar un contexto adicional sobre la navegación.
    
    - botón de menú: Se añaden atributos 'aria-expanded' y 'aria-controls'
      al botón de menú móvil, mejorando la accesibilidad para usuarios de
      lectores de pantalla.
    
    - texto alternativo: Se incluye un <span> con la clase 'sr-only' que
      indica 'Abrir menú', facilitando la comprensión del botón para
      usuarios con discapacidades.
    
    - roles de menú: Se establecen los roles 'menubar' y 'menuitem' en
      las listas de navegación, mejorando la estructura semántica y
      accesibilidad del menú.
    
    - ocultar elementos: Se utiliza 'aria-hidden' para indicar que el
      menú móvil está oculto por defecto, mejorando la experiencia del
      usuario al interactuar con la navegación.
    
    Estas mejoras refuerzan la accesibilidad y la usabilidad de la barra
    de navegación, contribuyendo a una mejor experiencia general del usuario.
    
    No hay issues
    #Close 0

--- resources/Views/layouts/partials/navbar.php
+++ resources/Views/layouts/partials/navbar.php
@@ -1,6 +1,6 @@
-<nav class="navbar fixed top-0 left-0 w-full z-10 bg-transparent text-white px-4 md:px-8 lg:px-16 py-3 transition-colors duration-300">
-    <div class="container mx-auto flex justify-between items-center">
-        <a href="<?= base_url('/') ?>" class="block text-lg font-bold relative" data-type="logo">
+<nav class="navbar fixed top-0 left-0 w-full z-10 bg-transparent text-white py-3 transition-colors duration-300" aria-label="Main navigation">
+    <div class="container mx-auto px-4 md:px-8 lg:px-16 xl:px-52 flex justify-between items-center">
+        <a href="<?= base_url('/') ?>" class="block text-lg font-bold relative" data-type="logo" aria-label="Inicio">
             <?php
             renderImage(
                 'logo_cnsv', 
@@ -11,27 +11,28 @@
             ?>
         </a>
         <div class="block md:hidden">
-            <button id="menu-toggle" class="text-white focus:outline-none">
+            <button id="menu-toggle" class="text-white focus:outline-none" aria-expanded="false" aria-controls="mobile-menu">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
                 </svg>
+                <span class="sr-only">Abrir menú</span>
             </button>
         </div>
-        <ul class="hidden md:flex-grow md:flex md:justify-center space-x-6">
-            <li><a href="<?= base_url('/') ?>" class="relative">Inicio</a></li>
-            <li><a href="<?= base_url('/about') ?>" class="relative">¿Quiénes somos?</a></li>
-            <li><a href="<?= base_url('/visit') ?>" class="relative">Visítanos</a></li>
-            <li><a href="<?= base_url('/donation') ?>" class="relative">Donaciones</a></li>
+        <ul class="hidden md:flex-grow md:flex md:justify-center space-x-6" role="menubar">
+            <li role="menuitem"><a href="<?= base_url('/') ?>" class="relative">Inicio</a></li>
+            <li role="menuitem"><a href="<?= base_url('/about') ?>" class="relative">¿Quiénes somos?</a></li>
+            <li role="menuitem"><a href="<?= base_url('/visit') ?>" class="relative">Visítanos</a></li>
+            <li role="menuitem"><a href="<?= base_url('/donation') ?>" class="relative">Donaciones</a></li>
         </ul>
         <div class="hidden md:block"><?php PrimaryButton('Admisión', 'admission');?></div>
     </div>
 
-    <div id="mobile-menu" class="hidden h-full bg-black transition-transform transform -translate-y-full duration-300">
+    <div id="mobile-menu" class="hidden h-full bg-black transition-transform transform -translate-y-full duration-300" role="menu" aria-hidden="true">
         <ul class="flex flex-col space-y-6 p-4 text-white">
-            <li><a href="<?= base_url('/') ?>" class="block relative">Inicio</a></li>
-            <li><a href="<?= base_url('/about') ?>" class="block relative">¿Quiénes somos?</a></li>
-            <li><a href="<?= base_url('/visit') ?>" class="block relative">Visítanos</a></li>
-            <li><a href="<?= base_url('/donation') ?>" class="block relative">Donaciones</a></li>
+            <li role="menuitem"><a href="<?= base_url('/') ?>" class="block relative">Inicio</a></li>
+            <li role="menuitem"><a href="<?= base_url('/about') ?>" class="block relative">¿Quiénes somos?</a></li>
+            <li role="menuitem"><a href="<?= base_url('/visit') ?>" class="block relative">Visítanos</a></li>
+            <li role="menuitem"><a href="<?= base_url('/donation') ?>" class="block relative">Donaciones</a></li>
             <li><?php PrimaryButton('Admisión', 'admission');?></li>
         </ul>
     </div>
