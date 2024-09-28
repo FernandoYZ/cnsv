799f9663ba3cfdea9d986ee4927724a50379a16d
FernandoYZ <fernandozavala266@gmail.com>
2024-09-28 08:05:09 -0500

    chore(config): simplificar configuración de la aplicación
    Se ha modificado el archivo 'config/app.php' para simplificar la configuración de la aplicación. Se han eliminado varias variables relacionadas con datos que no son críticos para la inicialización básica del sistema.
    
    Detalles de los cambios:
    
    - Se han eliminado las siguientes configuraciones:
      - description
      - address
      - property_url
      - property_type
      - property_title
      - property_description
      - location
      - keywords
      - facebook
      - email
      - phone
      - authors
    
    Este ajuste permite una gestión más clara y eficiente de la configuración de la aplicación, centrándose en los parámetros esenciales para su funcionamiento.
    
    No hay issues
    #Close 0

--- config/app.php
+++ config/app.php
@@ -8,32 +8,5 @@ return [
     'url' => env('APP_URL', 'http://localhost'),
     'currency' => env('APP_CURRENCY', 'PEN'),
     'language' => env('APP_LANGUAGE', 'es'),
-    'description' => env('DATA_DESCRIPTION', 'Description'),
-    'address' => env('DATA_ADDRESS', 'Calle, Provincia, Departamento'),
-    'property_url' => env('DATA_PROPERTY_URL', 'https://google.com'),
-    'property_type' => env('DATA_PROPERTY_TYPE', 'Meta Title'),
-    'property_title' => env('DATA_PROPERTY_TITLE', 'Meta Title'),
-    'property_description' => env('DATA_PROPERTY_DESCRIPTION', 'Meta Description'),
-    'location' => env('DATA_LOCATION', 'Lima, Perú'),
-    'keywords' => env('DATA_KEYWORDS', 'keywords'),
-    'facebook' => env('DATA_FACEBOOK_URL', 'https://facebook.com'),
-    'email' => env('DATA_CONTACT_EMAIL', 'email@email.com'),
-    'phone' => env('DATA_PHONE_NUMBER', '+51999999999'),
-    'authors' => [
-        [
-            'fullname' => env('AUTHOR_1_FULLNAME', 'Nombre Autor 1'),
-            'name' => env('AUTHOR_1_NAME', 'NombreAlternativo1'),
-            'email' => env('AUTHOR_1_EMAIL', 'email1@example.com'),
-        ],
-        [
-            'fullname' => env('AUTHOR_2_FULLNAME', 'Nombre Autor 2'),
-            'name' => env('AUTHOR_2_NAME', 'NombreAlternativo2'),
-            'email' => env('AUTHOR_2_EMAIL', 'email2@example.com'),
-        ],
-        [
-            'fullname' => env('AUTHOR_3_FULLNAME', 'Nombre Autor 3'),
-            'email' => env('AUTHOR_3_EMAIL', 'email3@example.com'),
-        ],
-    ],
     'company' => env('AUTHOR_COMPANY', 'Company'),
 ];
