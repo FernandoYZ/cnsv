3b182fccc1c71f467d65d2699abf8089b5ad67e4
FernandoYZ <fernandozavala266@gmail.com>
2024-09-28 08:31:27 -0500

    feat(hero): mejorar estructura y funcionalidad de secciones Hero
    Se han implementado cambios significativos en las funciones HeroSection y HeroHome:
    
    - HeroSection:
      - Se agregó la capacidad de aceptar un URI para el botón, permitiendo enlaces dinámicos.
      - Se mejoró la estructura de la sección, utilizando una imagen de fondo en lugar de CSS, para una mayor flexibilidad y accesibilidad.
      - Se añadió un atributo aria-labelledby para mejorar la accesibilidad.
      - Se optimizó el contenido del botón a través de una nueva función HeroButton.
    
    - HeroHome:
      - Se realizaron cambios similares a HeroSection, permitiendo la adición de un URI para el botón.
      - Se mejoró la semántica al incluir un contenedor para el título y el botón, garantizando una disposición responsiva y accesible.
    
    Estos cambios no solo mejoran la modularidad del código, sino que también garantizan una experiencia de usuario más atractiva y accesible.

--- resources/Views/components/hero.php
+++ resources/Views/components/hero.php
@@ -1,35 +1,46 @@
 <?php
-function HeroSection($image, $heroTitle, $textButton, $content = '') {
-    $bg = images("/{$image}");
-    $sourceContent = !empty($content) ? "<p class=\"text-lg lg:text-3xl md:text-xl mb-8\">{$content}</p>" : '';
+function HeroSection($image, $heroTitle, $content = '', $textButton = '', $uriButton = '') {
+    $bg = images("/hero/{$image}");
+    $button = !empty($textButton) && !empty($uriButton) ? HeroButton($textButton, $uriButton) : '';
+    $sourceContent = !empty($content) ? "<p class=\"text-lg md:text-xl lg:text-2xl font-normal mb-8\">{$content}</p>" : '';
 
     echo "
-        <section class=\"relative bg-cover bg-center h-screen\" style=\"background-image: url('{$bg}');\">
-            <div class=\"flex items-center justify-center h-full bg-black bg-opacity-50\">
-                <div class=\"text-center text-white p-4 md:p-6 lg:p-8 xl:p-10\">
-                    <h1 class=\"text-3xl md:text-5xl lg:text-7xl xl:text-8xl font-thin mb-8\">{$heroTitle}</h1>
-                    {$sourceContent}
-                    <a href=\"#\" class=\"bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded transition duration-300\">{$textButton}</a>
-                </div>
-            </div>
-        </section>
+    <section class=\"relative h-screen flex items-center justify-center\" aria-labelledby=\"who-we-are\">
+        <div class=\"absolute inset-0 z-0\">
+            <img
+                src=\"{$bg}\"
+                alt=\"Fondo hero de {$heroTitle}\"
+                class=\"w-full h-full object-cover\"
+            />
+            <div class=\"absolute inset-0 bg-black opacity-50\"></div> 
+        </div>
+        <div class=\"relative z-10 text-center text-white container mx-auto px-4 md:px-8 lg:px-16 xl:px-52\">
+            <h1 id=\"who-we-are\" class=\"text-3xl md:text-5xl lg:text-6xl xl:text-8xl font-thin mb-6\">{$heroTitle}</h1>
+            {$sourceContent}
+            {$button}
+        </div>
+    </section>
     ";
 }
 
-function HeroHome($image, $heroTitle, $textButton, $content = '') {
-    $bg = images("/{$image}");
-    $sourceContent = !empty($content) ? "<p class=\"text-lg md:text-xl mb-8\">{$content}</p>" : '';
+function HeroHome($image, $heroTitle, $textButton = '', $uriButton = '') {
+    $bg = images("/hero/{$image}");
+    $button = !empty($textButton) && !empty($uriButton) ? HeroButton($textButton, $uriButton) : '';
 
     echo "
-        <section class=\"relative bg-cover bg-center h-screen\" style=\"background-image: url('{$bg}');\">
-            <div class=\"flex items-center justify-center h-full bg-black bg-opacity-50\">
-                <div class=\"text-left text-white p-4 md:p-6 lg:p-8 xl:p-10\">
-                    <h1 class=\"text-3xl md:text-5xl lg:text-7xl xl:text-8xl font-thin mb-8\">{$heroTitle}</h1>
-                    {$sourceContent}
-                    <a href=\"#\" class=\"bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded transition duration-300\">{$textButton}</a>
-                </div>
-            </div>
-        </section>
+    <section class=\"relative h-screen flex items-center justify-center\" aria-labelledby=\"who-we-are\">
+        <div class=\"absolute inset-0 z-0\">
+            <img
+                src=\"{$bg}\"
+                alt=\"Fondo hero de {$heroTitle}\"
+                class=\"w-full h-full object-cover\"
+            />
+        </div>
+        <div class=\"relative z-10 container mx-auto px-4 md:px-8 lg:px-16 xl:px-52\">
+            <h1 id=\"who-we-are\" class=\"text-white text-3xl md:text-5xl lg:text-6xl xl:text-8xl font-thin mb-6 text-center md:text-left lg:max-w-[70%]\">{$heroTitle}</h1>
+            <div class=\"text-center md:text-left\">{$button}</div>
+        </div>
+    </section>
     ";
 }
 
