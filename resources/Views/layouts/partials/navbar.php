204a990ddd45158281ed0b4ab5aebd368e5342de
FernandoYZ <fernandozavala266@gmail.com>
2024-09-28 08:25:32 -0500

    refactor(navbar): mejorar accesibilidad y semántica
    Se han realizado las siguientes mejoras en la barra de navegación:
    
    - Se ha ajustado el nivel de z-index de z-10 a z-20 para asegurar que la barra de navegación permanezca por encima de otros elementos en la página.
    - Se ha añadido el atributo data-type='item' a los enlaces del menú, mejorando la accesibilidad y permitiendo un mejor manejo de eventos.
    
    Estos cambios contribuyen a una mejor experiencia de usuario y una navegación más intuitiva en el sitio.

--- resources/Views/layouts/partials/navbar.php
+++ resources/Views/layouts/partials/navbar.php
@@ -1,4 +1,4 @@
-<nav class="navbar fixed top-0 left-0 w-full z-10 bg-transparent text-white py-3 transition-colors duration-300" aria-label="Main navigation">
+<nav class="navbar fixed top-0 left-0 w-full z-20 bg-transparent text-white py-3 transition-colors duration-300" aria-label="Main navigation">
     <div class="container mx-auto px-4 md:px-8 lg:px-16 xl:px-52 flex justify-between items-center">
         <a href="<?= base_url('/') ?>" class="block text-lg font-bold relative" data-type="logo" aria-label="Inicio">
             <?php
@@ -19,14 +19,13 @@
             </button>
         </div>
         <ul class="hidden md:flex-grow md:flex md:justify-center space-x-6" role="menubar">
-            <li role="menuitem"><a href="<?= base_url('/') ?>" class="relative">Inicio</a></li>
-            <li role="menuitem"><a href="<?= base_url('/about') ?>" class="relative">¿Quiénes somos?</a></li>
-            <li role="menuitem"><a href="<?= base_url('/visit') ?>" class="relative">Visítanos</a></li>
-            <li role="menuitem"><a href="<?= base_url('/donation') ?>" class="relative">Donaciones</a></li>
+            <li role="menuitem"><a href="<?= base_url('/') ?>" data-type="item" class="relative">Inicio</a></li>
+            <li role="menuitem"><a href="<?= base_url('/about') ?>" data-type="item" class="relative">¿Quiénes somos?</a></li>
+            <li role="menuitem"><a href="<?= base_url('/visit') ?>" data-type="item" class="relative">Visítanos</a></li>
+            <li role="menuitem"><a href="<?= base_url('/donation') ?>" data-type="item" class="relative">Donaciones</a></li>
         </ul>
         <div class="hidden md:block"><?php PrimaryButton('Admisión', 'admission');?></div>
     </div>
-
     <div id="mobile-menu" class="hidden h-full bg-black transition-transform transform -translate-y-full duration-300" role="menu" aria-hidden="true">
         <ul class="flex flex-col space-y-6 p-4 text-white">
             <li role="menuitem"><a href="<?= base_url('/') ?>" class="block relative">Inicio</a></li>
