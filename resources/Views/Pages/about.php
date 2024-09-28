b3d10c25a034a6c310d73756942406e252380f72
FernandoYZ <fernandozavala266@gmail.com>
2024-09-28 08:42:32 -0500

    refactor(pages): actualizar contenido en la página about
    Se han realizado cambios en el archivo about.php para mejorar la estructura y contenido de la sección '¿Quiénes somos?'.
    
    Cambios realizados:
    - Se actualizó la imagen de fondo en la función HeroSection a about_hero.jpg.
    - Se modificó el contenido del encabezado para incluir directamente la descripción de la institución, eliminando el botón de donación.
    - Se implementó la función mision_vision, que encapsula la lógica para generar las secciones de misión y visión de la institución:
      - La función crea dos variables:
        - mission, que utiliza ArticleLeft para mostrar la misión con una imagen a la izquierda y texto descriptivo a la derecha.
        - ision, que emplea ArticleRight para presentar la visión con una imagen a la derecha y texto a la izquierda.
      - Ambas variables se combinan en un contenedor utilizando la función Container, la cual establece un fondo neutral (bg-neutral-50) y aplica un diseño responsivo.
    - Finalmente, se añade una llamada a HeroSection para presentar la organización con la imagen correspondiente.
    
    No hay issues
    #Close 0

--- resources/Views/Pages/about.php
+++ resources/Views/Pages/about.php
@@ -1,8 +1,36 @@
 <?=
 HeroSection(
-    'hero/about_hero.png',
+    'about_hero.jpg',
     '¿Quiénes somos?',
-    'Donar',
-    'Somos una Institución Educativa de Acción Conjunta de Educación Básica Regular entre la Iglesia Católica y el Estado Peruano, erigida como Colegio – Seminario Menor, '
+    'Somos una Institución Educativa de Acción Conjunta de Educación Básica Regular entre la Iglesia Católica y el Estado Peruano, erigida como Colegio – Seminario Menor, ',
+)
+?>
+
+<?php
+function mision_vision() {
+    $mission = ArticleLeft(
+        'Nuestra Misión',
+        images('/hero/home_hero_org.jpg'),
+        config('mission')
+    );
+    $vision = ArticleRight(
+        'Nuestra Visión',
+        images('/hero/home_hero_org.jpg'),
+        config('vision')
+    );
+    $container = Container(
+        "{$mission}{$vision}",
+        'bg-neutral-50',
+    );
+    return $container;
+}
+
+echo mision_vision();
+?>
+
+<?=
+HeroSection(
+    'about_organization.jpg',
+    'Nuestra organización',
 )
 ?>
