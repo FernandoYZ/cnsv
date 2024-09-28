ef4990685bbcc44cff92e4aca6863b75c6df7e62
FernandoYZ <fernandozavala266@gmail.com>
2024-09-28 08:04:26 -0500

    chore(config): incluir configuración de datos adicionales
    Se ha modificado el archivo 'bootstrap/app.php' para incluir la carga de la configuración de datos desde 'data.php'. Este cambio permite una integración más completa de la configuración de la aplicación, consolidando la gestión de variables necesarias para su funcionamiento.
    
    Detalles de los cambios:
    
    - Carga de Configuración: Se ha añadido la línea equire_once __DIR__ . '/../config/data.php'; para incluir la configuración de datos en el archivo de arranque de la aplicación.
    
    - Fusión de Configuraciones: Se ha actualizado la variable global $GLOBALS['config'] para incluir los datos de configuración de la nueva fuente, utilizando rray_merge para asegurar que todos los parámetros sean accesibles desde la configuración central de la aplicación.
    
    Este ajuste optimiza la flexibilidad y escalabilidad del sistema, permitiendo la incorporación de nuevas configuraciones sin afectar la estructura existente.
    
    No hay issues
    #Close 0

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,9 +14,10 @@ $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
 
 $config = require_once __DIR__ . '/../config/app.php';
+$dataConcep = require_once __DIR__ . '/../config/data.php';  
 $dbConfig = require_once __DIR__ . '/../config/database.php';
 
-$GLOBALS['config'] = array_merge($config, ['database' => $dbConfig]);
+$GLOBALS['config'] = array_merge($config, ['database' => $dbConfig], $dataConcep);
 
 $databaseConfig = $GLOBALS['config']['database'];
 
