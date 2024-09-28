d1d08ec195979408088d77a76c7cf18c76e53ec1
FernandoYZ <fernandozavala266@gmail.com>
2024-09-28 08:33:41 -0500

    feat(views): agregar nuevos componentes
    Se han realizado cambios en el archivo main.php para incluir nuevos componentes necesarios en las vistas:
    
    - Se ha agregado heroButton.php para permitir el uso de botones específicos dentro de las secciones hero.
    - Se ha incluido rticle.php, que proporciona la funcionalidad para renderizar artículos de manera flexible.
    - Se ha añadido container.php, que facilita la creación de contenedores personalizables en las vistas.
    
    Estos cambios mejoran la modularidad del código y permiten una mayor reutilización de componentes en la aplicación, contribuyendo a una estructura más organizada y mantenible.
    
    No hay issues
    #Close 0

--- resources/Views/components/main.php
+++ resources/Views/components/main.php
@@ -2,4 +2,7 @@
 
 require_once __DIR__ . '/images.php';
 require_once __DIR__ . '/primaryButton.php';
+require_once __DIR__ . '/heroButton.php';
 require_once __DIR__ . '/hero.php';
+require_once __DIR__ . '/article.php';
+require_once __DIR__ . '/container.php';
