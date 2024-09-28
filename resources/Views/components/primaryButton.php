cd1a39fa8f4783f51fe7f6e14aee03994fb89fa6
FernandoYZ <fernandozavala266@gmail.com>
2024-09-28 08:23:55 -0500

    feat(views): actualizar función PrimaryButton para utilizar enlaces
    Se ha modificado el archivo 'PrimaryButton.php' en 'resources/Views/components' para cambiar la implementación de un botón a un enlace estilizado, mejorando así la accesibilidad y la semántica del HTML.
    
    - 'PrimaryButton':
      - Acepta dos parámetros:
        - '': El texto que se mostrará dentro del botón.
        - '': La URI a la que redirigirá el enlace al ser clicado.
      - Se genera un elemento '<a>' en lugar de un '<button>', permitiendo una navegación más natural y accesible dentro de la aplicación.
      - Se ha añadido el atributo 'href' que utiliza la función 'base_url' para construir la URL de redirección.
      - Se incorpora el atributo 'role' con valor 'button' para mejorar la accesibilidad y permitir que los lectores de pantalla interpreten el enlace como un botón.
      - Se incluye el atributo 'aria-label' que proporciona información adicional sobre la acción que se realizará al clicar, mejorando la experiencia del usuario.
      - Se mantiene el estilo visual utilizando clases de TailwindCSS, asegurando que el enlace se comporte visualmente como un botón, conservando la interacción esperada.
    
    Esta actualización mejora la usabilidad y la accesibilidad de la aplicación, al tiempo que se mantiene la estética de diseño.
    
    No hay issues
    #Close 0

--- resources/Views/components/primaryButton.php
+++ resources/Views/components/primaryButton.php
@@ -1,15 +1,17 @@
 <?php
 
-function PrimaryButton ($content, $uri) {
+function PrimaryButton($content, $uri) {
     $redirect = base_url("/{$uri}");
 
     echo "
-    <button 
-        type=\"button\"
-        class=\"text-sm front-monserat py-2.5 px-3 border-[1px] border-white text-white hover:bg-white hover:border-white hover:text-neutral-900 transition\"
-        onclick=\"window.location.href='{$redirect}'\"
-        >
+    <a 
+    href=\"{$redirect}\"
+    class=\"text-sm font-monserat py-2.5 px-3 border border-white text-white hover:bg-white hover:border-white hover:text-neutral-900 transition\"
+    role=\"button\"
+    aria-label=\"Ir a {$content}\"
+    data-type=\"button-link\"
+    >
         {$content}
-    </button>
+    </a>
     ";
 }
