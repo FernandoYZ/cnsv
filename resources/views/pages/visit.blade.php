613974a1cbeba3b44d8b3849adcada03f4d97f0c
FernandoYZ <fernandozavala266@gmail.com>
2024-11-24 20:45:07 -0500

    feat(page): crear vista visit.blade.php
    Se ha creado la vista `visit.blade.php` en el directorio `resources/views/pages`, correspondiente a la página "Visítanos". Esta página utiliza el layout base `x-layouts.app`, lo que garantiza que todos los elementos globales, como la cabecera y el pie de página, se mantengan consistentes a lo largo de la aplicación.
    
    Dentro de la vista, se ha añadido una sección de ejemplo llamada `servicios`, que incluye:
    - La sección ocupa toda la altura de la pantalla (`h-screen`) y tiene un fondo gris claro (`bg-gray-300`).
    - El contenido está centrado vertical y horizontalmente utilizando clases como `flex`, `items-center` y `justify-center`.
    - Un título de sección con el estilo `text-4xl`.
    
    Este diseño proporciona una estructura base para la página "Visítanos", que puede ser fácilmente extendida o modificada en el futuro.
    
    Closes 0

---
+++ resources/views/pages/visit.blade.php
@@ -0,0 +1,7 @@
+<x-layouts.app title="Visítanos">
+    <section id="servicios" class="h-screen bg-gray-300 flex items-center justify-center">
+        <div class="sombra-superior"></div>
+        <h2 class="text-4xl">Sección de ejemplo</h2>
+    </section>
+</x-layouts.app>
+
