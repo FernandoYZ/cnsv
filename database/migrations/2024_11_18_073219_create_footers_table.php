163b18dca3393fbd5749178a45196699178e3596
FernandoYZ <fernandozavala266@gmail.com>
2024-11-24 19:55:52 -0500

    feat(modelo, migración): crear modelo Footer con migración para la tabla `footers`
    Se ha creado el modelo `Footer` junto con su migración para la tabla `footers`, que almacenará la información de contacto y detalles adicionales del pie de página de la aplicación, como dirección, teléfonos y correos electrónicos.
    
    - Se creó el modelo `Footer` en `app/Models/Footer.php`.
    - Se generó la migración correspondiente para la tabla `footers` con los siguientes campos:
      - `address` (string, nullable)
      - `telephone` (string, nullable)
      - `cell_phone` (string, nullable)
      - `email_primary` (string, nullable)
      - `email_secondary` (string, nullable)
      - Timestamps (`created_at`, `updated_at`)
    
    #Closes 0

---
+++ database/migrations/2024_11_18_073219_create_footers_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('footers', function (Blueprint $table) {
+            $table->id();
+            $table->string('address')->nullable();
+            $table->string('telephone')->nullable();
+            $table->string('cell_phone')->nullable();
+            $table->string('email_primary')->nullable();
+            $table->string('email_secondary')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('footers');
+    }
+};
