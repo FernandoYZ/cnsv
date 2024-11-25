37bbbd68d0d5c29af4e698e5649c1461f6b99501
FernandoYZ <fernandozavala266@gmail.com>
2024-11-24 20:15:12 -0500

    feat(seeder): agregar siembra de `Footer` y `Config` en `DatabaseSeeder`
    Se ha actualizado el archivo `DatabaseSeeder.php` para incluir la siembra de datos predeterminados para los modelos `Footer` y `Config`. Esto facilita la carga de datos iniciales esenciales en la base de datos.
    
    - Se agregó la creación de un `Footer` con datos de ejemplo, como la dirección, teléfono y correo electrónico primario.
    - Se añadió la creación de un `Config` con la configuración inicial del colegio, incluyendo nombre, palabras clave, URL, título y descripción de la propiedad, entre otros.
    
    #Closes 0

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,5 +16,22 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        // Footer::factory(1)->create();
+
+        Footer::factory()->create([
+            'address' => 'Carretera a Lunahuana km. 8, Apdo. 69, Nuevo Imperial, Peru',
+            'cell_phone' => '+51 940 768 184',
+            'email_primary' => 'cnsv.rectorado@gmail.com',
+        ]);
+        Config::factory()->create([
+            'name'=>'CNSV - Colegio Nuestra Señora del Valle',
+            'keyword'=>'educación primaria, educación secundaria, seminario menor, educación religiosa, valores, formación integral, colegio con residencia, colegio internado',
+            'property_url'=>'https://cnsv.edu.pe',
+            'property_type'=>'website',
+            'property_title'=>'Colegio Nuestra Señora del Valle Seminario Menor',
+            'property_description'=>'Brindamos formación académica y valores en un ambiente integral y humano, promoviendo el desarrollo integral de nuestros estudiantes.',
+            'meta_description'=>'En el Colegio Nuestra Señora del Valle, brindamos educación integral y formación en valores en modalidad de residencia.',
+            'meta_author'=>'Yovera Zavala Fernando Cesar',
+        ]);
     }
 }
