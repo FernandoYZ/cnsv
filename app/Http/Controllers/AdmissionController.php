974312634e3ee546c723bc9ec798c7fd291945d0
FernandoYZ <fernandozavala266@gmail.com>
2024-11-24 19:41:23 -0500

    refactor(controller): actualizar AdmissionController a la estructura de Laravel
    El controlador `AdmissionController` ha sido reestructurado para seguir las convenciones de Laravel, aprovechando el comando Artisan `php artisan make:controller AdmissionController -r` para generar la estructura de controlador de recursos.
    
    Cambios realizados:
    - El controlador ahora utiliza el método `view()` de Laravel para retornar directamente la vista `pages.admission`.
    - Se eliminó la lógica personalizada para el manejo de vistas y datos, alineándose con la estructura simplificada y coherente de Laravel.
    - Se generaron los métodos estándar de controlador de recursos (`create()`, `store()`, `edit()`, `update()`, etc.) como parte de la plantilla, aunque no se implementan aún.
    
    Este cambio mejora la consistencia con las prácticas recomendadas de Laravel y facilita la integración con el resto del framework, permitiendo un manejo más eficiente de las rutas y vistas.
    
    #Closes 0

--- app/Http/Controllers/AdmissionController.php
+++ app/Http/Controllers/AdmissionController.php
@@ -2,14 +2,63 @@
 
 namespace App\Http\Controllers;
 
-use Core\Controller;
+use Illuminate\Http\Request;
 
-class AdmissionController extends Controller {
-    public function index() {
-        $data = [
-            'title' => 'Admisión',
-            'description' => 'Infórmate sobre el proceso de admisión en el Colegio Nuestra Señora del Valle y cómo formar parte de nuestra comunidad educativa.',
-        ];
-        return $this->views->getView('Pages', 'admission', $data);
+class AdmissionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('pages.admission');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
     }
 }
