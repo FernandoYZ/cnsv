3a0deb04edc4c99dd7b6e8cf9d19ba03f68ef2e9
FernandoYZ <fernandozavala266@gmail.com>
2024-11-24 20:44:40 -0500

    feat(page): crear vista home.blade.php
    Se ha creado la vista `home.blade.php` en el directorio `resources/views/pages`, correspondiente a la página principal o "Home". Esta página utiliza el layout base `x-layouts.app`, asegurando que la cabecera, pie de página y demás elementos globales se mantengan consistentes con el resto de la aplicación.
    
    Dentro de la vista, se ha añadido una sección de ejemplo llamada `servicios`, que tiene las siguientes características:
    - La sección ocupa toda la altura de la pantalla (`h-screen`) y tiene un fondo gris claro (`bg-gray-300`).
    - El contenido está centrado tanto vertical como horizontalmente con las clases `flex`, `items-center`, y `justify-center`.
    - Se incluye un título de sección con un tamaño de texto `text-4xl`.
    
    Esta estructura permite que la página principal se integre de manera fluida con el diseño general de la aplicación, y proporciona un espacio para agregar contenido adicional en el futuro.
    
    Closes 0

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,6 @@
+<x-layouts.app title="Inicio">
+    <section id="servicios" class="h-screen bg-gray-300 flex items-center justify-center">
+        <div class="sombra-superior"></div>
+        <h2 class="text-4xl">Sección de ejemplo</h2>
+    </section>
+</x-layouts.app>
