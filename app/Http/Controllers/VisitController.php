77b306f8d89a2ab915f26bd9dcaa77e3cf56aad7
FernandoYZ <fernandozavala266@gmail.com>
2024-11-24 19:51:01 -0500

    refactor(controller): actualizar VisitController
    Refactoriza `VisitController` para alinearlo con la estructura estándar de un controlador de recursos en Laravel, utilizando el comando `php artisan make:controller VisitController -r`. Este cambio organiza el controlador de forma más clara y sigue las mejores prácticas de Laravel para el manejo de recursos.
    
    - Se elimina la dependencia de `Core\Controller` y la gestión manual de vistas, adoptando el controlador de recursos estándar de Laravel.
    - El método `index` ahora utiliza `view("pages.visit")` para devolver la vista correspondiente de manera más sencilla, aprovechando el sistema de vistas de Laravel.
    - Se agregan los métodos estándar de un controlador de recursos como `create`, `store`, `show`, `edit`, `update`, `destroy`, para garantizar la consistencia con el resto de controladores de la aplicación.
    - Se incluye el uso de la clase `Request` en lugar de una implementación personalizada, facilitando la manipulación de solicitudes HTTP.
    
    #Closes 0

--- app/Http/Controllers/VisitController.php
+++ app/Http/Controllers/VisitController.php
@@ -2,14 +2,63 @@
 
 namespace App\Http\Controllers;
 
-use Core\Controller;
+use Illuminate\Http\Request;
 
-class VisitController extends Controller {
-    public function index() {
-        $data = [
-            'title' => 'Visítanos',
-            'description' => 'Visítanos en el Colegio Nuestra Señora del Valle y descubre nuestra propuesta educativa en un entorno residencial.',
-        ];
-        return $this->views->getView('Pages', 'visit', $data);
+class VisitController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('pages.visit');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
     }
 }
