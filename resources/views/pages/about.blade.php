f9ab3010efb0566aa4a467459fded12767abf644
FernandoYZ <fernandozavala266@gmail.com>
2024-11-24 20:42:31 -0500

    feat(page): crear vista sobre-nosotros.blade.php
    Se ha creado la vista `about.blade.php` en el directorio `resources/views/pages`, que corresponde a la página "Sobre Nosotros". Esta vista utiliza el layout base `x-layouts.app` para asegurar una estructura común con cabecera y pie de página en toda la aplicación.
    
    Dentro de esta vista, se ha añadido una sección de ejemplo llamada `servicios`, la cual tiene el siguiente comportamiento:
    - La sección ocupa toda la altura de la pantalla (`h-screen`), con un fondo gris claro (`bg-gray-300`).
    - Se centra el contenido tanto vertical como horizontalmente usando las clases `flex`, `items-center`, y `justify-center`.
    - Se ha incluido un título en la sección con un texto de tamaño 4xl.
    
    Este enfoque modular permite que todas las páginas compartan un diseño consistente mientras se puede modificar el contenido específico de cada página de manera independiente.
    
    Closes 0

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,7 @@
+<x-layouts.app title="Sobre Nosotros">
+    <section id="servicios" class="h-screen bg-gray-300 flex items-center justify-center">
+        <div class="sombra-superior"></div>
+        <h2 class="text-4xl">Sección de ejemplo</h2>
+    </section>
+</x-layouts.app>
+
