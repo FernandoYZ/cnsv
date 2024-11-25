fecf2e6238d5bbbf1b1b793c0cdb4bf7d3b09671
FernandoYZ <fernandozavala266@gmail.com>
2024-11-24 19:31:13 -0500

    chore(views): eliminar vistas estáticas y migrar a Blade
    Se eliminan múltiples vistas estáticas en PHP y componentes que formaban parte de la estructura anterior del proyecto MVC. Esto se hace para adoptar el sistema de plantillas de Laravel, que utiliza Blade para manejar vistas dinámicas y reutilizables.
    
    - Se eliminan vistas de error personalizadas:
      - 404.php, 500.php, que ahora serán gestionadas por las vistas de errores de Laravel.
    
    - Se eliminan vistas de páginas estáticas que estaban siendo gestionadas como archivos PHP:
      - bout.php, dmission.php, donation.php, home.php, isit.php.
    
    - Se eliminan componentes reutilizables que anteriormente eran archivos PHP estáticos:
      - Componentes como rticle.php, card.php, container.php, eatures.php, hero.php, heroButton.php, images.php, main.php, primaryButton.php, 	estimonials.php.
    
    - Se eliminan archivos de la estructura de layout que eran responsables de manejar las secciones comunes como el pie de página, cabecera, y barra de navegación:
      - ooter.php, header.php,
    avbar.php, y el copyright.php que estaban dentro de la carpeta partials.
    
    Este cambio establece la transición de la estructura de vistas estáticas a Blade, lo que mejora la organización, reutilización y mantenibilidad de las vistas de la aplicación.
    
    #Closes 0

--- resources/Views/layouts/partials/copyright.php
+++
@@ -1,9 +0,0 @@
-<footer class="font-roboto bg-neutral-950 text-neutral-400 py-6" role="contentinfo">
-    <div class="container mx-auto px-4 md:px-8 lg:px-16 xl:px-52">
-        <p class="text-center md:text-left text-xs">
-            &copy; 2024 <?= config('property_title') ?>. Todos los derechos reservados. 
-            <span>Realizado por</span> 
-            <a href="#" class="font-semibold hover:transition-all hover:text-white" rel="noopener noreferrer"><?= config('company') ?></a>
-        </p>
-    </div>
-</footer>
--- resources/Views/layouts/partials/header.php
+++
@@ -1,3 +0,0 @@
-<header>
-    <?php include layout('partials.navbar'); ?>
-</header>
