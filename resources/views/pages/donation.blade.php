e6a2848591f53fe44f1b7981d5cce6873e64c044
FernandoYZ <fernandozavala266@gmail.com>
2024-11-24 20:44:06 -0500

    feat(page): crear vista donation.blade.php
    Se ha creado la vista `donation.blade.php` en el directorio `resources/views/pages`, correspondiente a la página de "Donación". Esta página utiliza el layout base `x-layouts.app`, lo que asegura una estructura común con cabecera y pie de página.
    
    Dentro de la vista, se ha añadido una sección de ejemplo llamada `servicios`, con las siguientes características:
    - La sección ocupa toda la altura de la pantalla (`h-screen`) y tiene un fondo gris claro (`bg-gray-300`).
    - El contenido está centrado tanto vertical como horizontalmente con las clases `flex`, `items-center`, y `justify-center`.
    - Se ha incluido un título en la sección con un tamaño de texto `text-4xl`.
    
    Esta estructura permite que la página de donación se integre de manera coherente con el diseño general de la aplicación, y el contenido puede ser modificado según sea necesario.
    
    Closes 0

---
+++ resources/views/pages/donation.blade.php
@@ -0,0 +1,6 @@
+<x-layouts.app title="Donación">
+    <section id="servicios" class="h-screen bg-gray-300 flex items-center justify-center">
+        <div class="sombra-superior"></div>
+        <h2 class="text-4xl">Sección de ejemplo</h2>
+    </section>
+</x-layouts.app>
