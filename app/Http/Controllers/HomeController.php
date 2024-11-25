209505d3e31b3d155a1d57516ec97013dcd40f87
FernandoYZ <fernandozavala266@gmail.com>
2024-11-24 19:49:52 -0500

    refactor(controller): actualizar HomeController
    Refactoriza `HomeController` para seguir la estructura de controlador de recursos estándar de Laravel, utilizando el comando `php artisan make:controller HomeController -r`. Este cambio mejora la consistencia y aprovecha las funcionalidades de Laravel para el manejo de recursos, permitiendo una estructura más limpia y coherente con el resto de controladores.
    
    - Se elimina la dependencia de `Core\Controller` y la gestión manual de vistas, adoptando el controlador de recursos de Laravel.
    - El método `index` ahora utiliza `view("pages.home")` para devolver la vista correspondiente, aprovechando el sistema de vistas de Laravel.
    - Se agregan los métodos estándar de un controlador de recursos como `create`, `store`, `show`, `edit`, `update`, `destroy`, en lugar de mantener una estructura manual.
    - Se añade la clase `Request` en lugar de utilizar una implementación personalizada para manejar los datos de las solicitudes (aunque por ahora los métodos no tienen implementación).
    
    #Closes 0

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,64 @@
 
 namespace App\Http\Controllers;
 
-use Core\Controller;
+use App\Models\Footer;
+use Illuminate\Http\Request;
 
-class HomeController extends Controller {
-    public function index() {
-        $data = [];
-        return $this->views->getView('Pages', 'home', $data);
+class HomeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('pages.home');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
     }
 }
