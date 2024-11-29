c37e49894ad2df1fa93896678087470347de7e2a
FernandoYZ <fernandozavala266@gmail.com>
2024-11-28 22:50:31 -0500

    feat(page): mejorar vista donation con secciones de donación y contribución
    Se ha mejorado la vista `donation.blade.php` en el directorio `resources/views/pages`, agregando contenido y secciones para fomentar las donaciones a la institución.
    
    Los cambios realizados son los siguientes:
    - Se ha añadido un componente `x-hero-section` con un título llamativo, invitando a los usuarios a ser parte del crecimiento de la institución.
    - Se ha creado una nueva sección con el título "Contribuyendo al colegio", donde se explica cómo las donaciones benefician a la institución.
    - Se han añadido tres tarjetas (`x-secundary-card`), describiendo los tipos de donaciones que se pueden realizar: Becas educativas, Tecnología educativa y Programas de mentoría.
    - Se ha añadido una nueva sección con un mensaje motivacional, invitando a los usuarios a apoyar al colegio y destacando el impacto positivo de sus contribuciones.
    - Se han mantenido las secciones de "Acerca de Nosotros" y "Contacto".
    
    Con estos cambios, la página de donaciones presenta un enfoque más claro y atractivo para alentar a los usuarios a contribuir a la causa de la institución.
    
    Closes 0

--- resources/views/pages/donation.blade.php
+++ resources/views/pages/donation.blade.php
@@ -1,6 +1,49 @@
-<x-layouts.app title="Donación">
-    <section id="servicios" class="h-screen bg-gray-300 flex items-center justify-center">
-        <div class="sombra-superior"></div>
-        <h2 class="text-4xl">Sección de ejemplo</h2>
+<x-layouts.app title="Donaciones">
+    <x-hero-section
+        nombreImagen="donation"
+        titulo="Sé parte del crecimiento de nuestra institución"
+        contenido=""
+        textButton=""
+        uriButton=""
+    />
+
+    <x-content class="bg-gray-100 py-8 lg:py-12">
+        <header class="text-center">
+            <h2 class="text-3xl lg:text-4xl font-medium mb-8">Contribuyendo al colegio</h2>
+            <p class="text-gray-600 lg:px-48">Descubre cómo tus donaciones pueden marcar la diferencia en la vida de nuestros estudiantes</p>
+        </header>
+        <main class="py-6 grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-10">
+            <x-secundary-card titulo="Becas educativas" icono="beca-card.svg">
+                Tus donaciones ayudan a proporcionar becas a estudiantes talentosos con recursos limitados.
+            </x-secundary-card>
+            <x-secundary-card titulo="Tecnología educativa" icono="computadora-card.svg">
+                Con tu apoyo, podemos mejorar nuestras instalaciones tecnológicas para un aprendizaje más efectivo.
+            </x-secundary-card>
+            <x-secundary-card titulo="Programas de mentoría" icono="grupo-card.svg">
+                Las donaciones financian programas de mentoría que conectan a estudiantes con profesionales experimentados.
+            </x-secundary-card>
+        </main>
+    </x-content>
+
+    <x-content class="bg-gray-100">
+        <div>
+
+        </div>
+        <main class="grid grid-cols-2 gap-6">
+            <div>
+                <h2 class="font-semibold text-5xl">Apoya al colegio</h2>
+                <span class="font-semibold text-lg">Tu contribución importa</span>
+                <p class="text-sm">Contribuye a la educación de calidad de nuestros estudiantes. Tus donaciones nos ayudan a mejorar las instalaciones, ofrecer más becas y asegurar un entorno de aprendizaje óptimo para todos. Únete a nuestra causa y sé parte del cambio.</p>
+            </div>
+            <div>Pene 2</div>
+        </main>
+    </x-content>
+
+    <section id="acerca" class="h-screen bg-gray-400 flex items-center justify-center">
+        <h2 class="text-4xl">Acerca de Nosotros</h2>
+    </section>
+    <section id="contacto" class="h-screen bg-gray-500 flex items-center justify-center">
+        <h2 class="text-4xl">Contacto</h2>
     </section>
 </x-layouts.app>
+
