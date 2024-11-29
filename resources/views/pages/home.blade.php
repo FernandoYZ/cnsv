8d5bf93bc396b7b5aec62223c652086fbd570c90
FernandoYZ <fernandozavala266@gmail.com>
2024-11-28 22:51:15 -0500

    feat(page): actualizar vista home con secciones
    Se ha actualizado la vista `home.blade.php` en el directorio `resources/views/pages`, agregando secciones informativas sobre la institución y el proceso de admisión.
    
    Los cambios realizados son los siguientes:
    - Se ha añadido un componente `x-hero-home` para presentar el "Proceso de admisión 2025", con un botón para redirigir a los usuarios a la página de admisión.
    - Se han añadido tres secciones `x-hero-section` con información sobre la institución, destacando su misión y objetivos, cada una con un botón para iniciar el proceso de admisión.
    - Se ha añadido una sección de "Nuestros Servicios", con un título destacado.
    - Se han mantenido las secciones de "Acerca de Nosotros" y "Contacto".
    
    Con estos cambios, la página de inicio ahora presenta un enfoque más completo para informar sobre el proceso de admisión y los servicios de la institución.
    
    Closes 0

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,6 +1,40 @@
 <x-layouts.app title="Inicio">
+    <x-hero-home
+        imagen="home_hero"
+        titulo="Proceso de admisión 2025"
+        uriButton="{{ route('admission') }}"
+        textButton="Inicia ya!"
+    />
+    <x-hero-section
+        nombreImagen="about_hero"
+        titulo="¿Quiénes somos?"
+        contenido="Somos una Institución Educativa de Acción Conjunta de Educación Básica Regular entre la Iglesia Católica y el Estado Peruano, erigida como Colegio - Seminario Menor."
+        textButton="Inicia ya!"
+        uriButton="{{ route('admission') }}"
+    />
+    <x-hero-section
+        nombreImagen="about_hero"
+        titulo="¿Quiénes somos?"
+        contenido="Somos una Institución Educativa de Acción Conjunta de Educación Básica Regular entre la Iglesia Católica y el Estado Peruano, erigida como Colegio - Seminario Menor."
+        textButton="Inicia ya!"
+        uriButton="{{ route('admission') }}"
+    />
+    <x-hero-section
+        nombreImagen="about_hero"
+        titulo="¿Quiénes somos?"
+        contenido="Somos una Institución Educativa de Acción Conjunta de Educación Básica Regular entre la Iglesia Católica y el Estado Peruano, erigida como Colegio - Seminario Menor."
+        textButton="Inicia ya!"
+        uriButton="{{ route('admission') }}"
+    />
+
     <section id="servicios" class="h-screen bg-gray-300 flex items-center justify-center">
-        <div class="sombra-superior"></div>
-        <h2 class="text-4xl">Sección de ejemplo</h2>
+        <h2 class="text-4xl">Nuestros Servicios</h2>
+    </section>
+
+    <section id="acerca" class="h-screen bg-gray-400 flex items-center justify-center">
+        <h2 class="text-4xl">Acerca de Nosotros</h2>
+    </section>
+    <section id="contacto" class="h-screen bg-gray-500 flex items-center justify-center">
+        <h2 class="text-4xl">Contacto</h2>
     </section>
 </x-layouts.app>
