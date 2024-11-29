e78fbc7c48aefd134025a58c546e9829113a1376
FernandoYZ <fernandozavala266@gmail.com>
2024-11-28 22:49:46 -0500

    feat(page): actualizar vista admission con componentes establecidos
    Se ha actualizado la vista `admission.blade.php` en el directorio `resources/views/pages`, agregando nuevas secciones y contenido relevante para la página de admisión.
    
    Los cambios realizados son los siguientes:
    - Se ha mantenido la sección de "Nuestros Servicios" en la parte superior, ahora con un título actualizado.
    - Se ha añadido una nueva sección con el título "Documentos a entregar", donde se describen los documentos necesarios para el proceso de admisión.
    - Se ha implementado un diseño de tarjetas (`x-primary-card`) para presentar cada documento requerido, con iconos visuales que mejoran la comprensión.
    - Se han añadido secciones de "Acerca de Nosotros" y "Contacto" con estilo de fondo gris para completar la página.
    
    Con estos cambios, la página de admisión ahora incluye una presentación más clara de los documentos necesarios y una estructura más organizada.
    
    Closes 0

--- resources/views/pages/admission.blade.php
+++ resources/views/pages/admission.blade.php
@@ -1,6 +1,30 @@
 <x-layouts.app title="Admisión">
     <section id="servicios" class="h-screen bg-gray-300 flex items-center justify-center">
-        <div class="sombra-superior"></div>
-        <h2 class="text-4xl">Sección de ejemplo</h2>
+        <h2 class="text-4xl">Nuestros Servicios</h2>
+    </section>
+
+    <x-content class="bg-gray-100 py-8 lg:py-24">
+        <header class="text-center">
+            <h2 class="text-3xl lg:text-4xl font-medium mb-8">Documentos a entregar</h2>
+        </header>
+        <main class="py-6 grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-10">
+            <x-primary-card titulo="" icono="documento-card.svg">
+                Ficha de inscripción rellenada y firmada por el Párroco, sacerdote o religiosa
+            </x-primary-card>
+            <x-primary-card titulo="" icono="copia-card.svg">
+                Fotocopia de libreta de calificaciones
+            </x-primary-card>
+            <x-primary-card titulo="" icono="camara-card.svg">
+                Foto reciente de tamaño carnet del estudiante con fondo blanco
+            </x-primary-card>
+        </main>
+    </x-content>
+
+    <section id="acerca" class="h-screen bg-gray-400 flex items-center justify-center">
+        <h2 class="text-4xl">Acerca de Nosotros</h2>
+    </section>
+    <section id="contacto" class="h-screen bg-gray-500 flex items-center justify-center">
+        <h2 class="text-4xl">Contacto</h2>
     </section>
 </x-layouts.app>
+
