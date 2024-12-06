6598769ac7c3f310ee91f088464b92656f856b22
FernandoYZ <fernandozavala266@gmail.com>
2024-12-06 06:59:46 -0500

    feat(component): crear plantilla BankCard con propiedades dinámicas
    Se ha creado la plantilla `bank-card.blade.php` para mostrar la información de una cuenta bancaria. La plantilla utiliza las propiedades `$titulo`, `$cuenta` y `$cci`, que se pasan dinámicamente desde el controlador. Además, se ha estructurado utilizando clases de Tailwind CSS para el diseño.
    
    #Closes 0

---
+++ resources/views/components/bank-card.blade.php
@@ -0,0 +1,13 @@
+<article class="px-8 py-6 rounded-xl border border-wine-600">
+    <header class="text-2xl text-wine-600 font-semibold mb-4">{{ $titulo }}</header>
+    <main class="flex justify-between items-center text-white">
+        <div class="space-y-2">
+            <h5 class="font-semibold text-lg">Cuenta Corriente</h5>
+            <p class="text-sm">{{ $cuenta }}</p>
+        </div>
+        <div class="space-y-2">
+            <h5 class="font-semibold text-lg">CCI</h5>
+            <p class="text-sm">{{ $cci }}</p>
+        </div>
+    </main>
+</article>
